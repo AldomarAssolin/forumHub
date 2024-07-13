9e74ff8451e3e2d99fc25501edd4628ada82d874
Aldomar Assolin <assolinaldomar@gmail.com>
2024-07-13 14:45:06 -0300

    Initial commit

---
+++ src/test/java/tech/aldomarassolin/forumHub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.aldomarassolin.forumHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
